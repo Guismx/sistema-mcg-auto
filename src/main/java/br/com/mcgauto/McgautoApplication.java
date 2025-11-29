958c2f1a226bf599355dc74a215fd88dd0c8e159
Guilherme Rodrigues de Almeida <guilherme.almeida@serpro.gov.br>
2025-11-29 01:35:56 -0300

    subindo projeto

---
+++ src/main/java/br/com/mcgauto/McgautoApplication.java
@@ -0,0 +1,13 @@
+package br.com.mcgauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McgautoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McgautoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mcgauto/McgautoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mcgauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McgautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
