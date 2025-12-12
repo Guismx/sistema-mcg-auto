f3122a25c53446c04f421c9635d3a79abb3c31b7
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2025-12-12 00:06:21 -0300

    fix: Ajuste getters e setters e adição do toString

--- src/main/java/br/com/mcgauto/domain/entities/Produto.java
+++ src/main/java/br/com/mcgauto/domain/entities/Produto.java
@@ -1,9 +1,6 @@
 package br.com.mcgauto.domain.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,23 +9,144 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+@Table(name = "Produto")
 public class Produto {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private long id_produto;
-    private int categoria_id;
-    private int codigo_produto;
+    private long idProduto;
+    private int categoriaid;
+    private int codigoProduto;
     private String nome;
     private String descricao;
     private String marca;
-    private BigDecimal preco_custo;
-    private BigDecimal preco_venda;
-    private int qtd_estoque;
-    private boolean is_ativo; //TODO: Alterar tipo depois
+    private BigDecimal precoCusto;
+    private BigDecimal precoVenda;
+    private int qtdEstoque;
+    private boolean isAtivo; //TODO: Alterar tipo depois
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public Produto() {
+
+    }
+
+    public Produto(long idProduto, int categoriaid, int codigoProduto, String nome, String descricao, String marca,
+                   BigDecimal precoCusto, BigDecimal precoVenda, int qtdEstoque, boolean isAtivo, LocalDateTime createdAt,
+                   LocalDateTime updatedAt) {
+        this.idProduto = idProduto;
+        this.categoriaid = categoriaid;
+        this.codigoProduto = codigoProduto;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.marca = marca;
+        this.precoCusto = precoCusto;
+        this.precoVenda = precoVenda;
+        this.qtdEstoque = qtdEstoque;
+        this.isAtivo = isAtivo;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public long getIdProduto() {
+        return idProduto;
+    }
+
+    public int getCategoriaid() {
+        return categoriaid;
+    }
+
+    public void setCategoriaid(int categoriaid) {
+        this.categoriaid = categoriaid;
+    }
+
+    public int getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    public void setCodigoProduto(int codigoProduto) {
+        this.codigoProduto = codigoProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public BigDecimal getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(BigDecimal precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public BigDecimal getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(BigDecimal precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public int getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public void setQtdEstoque(int qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public boolean isAtivo() {
+        return isAtivo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        isAtivo = ativo;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "idProduto=" + idProduto +
+                ", categoriaid=" + categoriaid +
+                ", codigoProduto=" + codigoProduto +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", marca='" + marca + '\'' +
+                ", precoCusto=" + precoCusto +
+                ", precoVenda=" + precoVenda +
+                ", qtdEstoque=" + qtdEstoque +
+                ", isAtivo=" + isAtivo +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
