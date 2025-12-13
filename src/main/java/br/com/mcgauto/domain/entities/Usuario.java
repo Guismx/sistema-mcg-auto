601ffd6cce7a0891e435bcd51c8e9895ab895ce8
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2025-12-13 16:39:44 -0300

    fix: removendo anotações Lombok e adicionado construtores e getters e setters

--- src/main/java/br/com/mcgauto/domain/entities/Usuario.java
+++ src/main/java/br/com/mcgauto/domain/entities/Usuario.java
@@ -1,9 +1,6 @@
 package br.com.mcgauto.domain.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +9,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Table(name = "Usuario")
 public class Usuario {
 
     @Id
@@ -30,4 +25,98 @@ public class Usuario {
     //private TipoConta tipoConta;
     private LocalDateTime dataCadastro;
     private boolean emailValidado;
+
+    public Usuario(){
+
+    }
+
+    public Usuario(long idUsuario, String nome, String sobrenome, Date dataNascimento, String telefonePrincipal, String telefoneSecundario,
+                   String email, String senha, LocalDateTime dataCadastro, boolean emailValidado) {
+        this.idUsuario = idUsuario;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.dataNascimento = dataNascimento;
+        this.telefonePrincipal = telefonePrincipal;
+        this.telefoneSecundario = telefoneSecundario;
+        this.email = email;
+        this.senha = senha;
+        this.dataCadastro = dataCadastro;
+        this.emailValidado = emailValidado;
+    }
+
+    public long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefonePrincipal() {
+        return telefonePrincipal;
+    }
+
+    public void setTelefonePrincipal(String telefonePrincipal) {
+        this.telefonePrincipal = telefonePrincipal;
+    }
+
+    public String getTelefoneSecundario() {
+        return telefoneSecundario;
+    }
+
+    public void setTelefoneSecundario(String telefoneSecundario) {
+        this.telefoneSecundario = telefoneSecundario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public boolean isEmailValidado() {
+        return emailValidado;
+    }
+
+    public void setEmailValidado(boolean emailValidado) {
+        this.emailValidado = emailValidado;
+    }
 }
