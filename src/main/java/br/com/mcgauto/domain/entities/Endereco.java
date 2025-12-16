a9b28960a4d3d0408601990a76902a7bc6e07f58
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2025-12-15 22:46:38 -0300

    feat: Adição classe de Endereco

---
+++ src/main/java/br/com/mcgauto/domain/entities/Endereco.java
@@ -0,0 +1,131 @@
+package br.com.mcgauto.domain.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "enderecos")
+public class Endereco {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long idEndereco;
+    private Usuario proprietario;
+    private TipoEndereco tipoEndereco;
+    private String logradouro;
+    private int numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String cep;
+    private boolean ativo;
+    //TODO: Implementar UF
+
+    public Endereco(){}
+
+    public Endereco(long idEndereco, Usuario proprietario, TipoEndereco tipoEndereco, String logradouro, int numero,
+                    String complemento, String bairro, String cidade, String cep, boolean ativo) {
+        this.idEndereco = idEndereco;
+        this.proprietario = proprietario;
+        this.tipoEndereco = tipoEndereco;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.cep = cep;
+        this.ativo = ativo;
+    }
+
+    public long getIdEndereco() {
+        return idEndereco;
+    }
+
+    public Usuario getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(Usuario proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    public TipoEndereco getTipoEndereco() {
+        return tipoEndereco;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "idEndereco=" + idEndereco +
+                ", proprietario=" + proprietario +
+                ", tipoEndereco=" + tipoEndereco +
+                ", logradouro='" + logradouro + '\'' +
+                ", numero=" + numero +
+                ", complemento='" + complemento + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", cep='" + cep + '\'' +
+                ", ativo=" + ativo +
+                '}';
+    }
+}
