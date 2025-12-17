270636af58bda37d7241cfbe427250475b997ceb
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2025-12-16 23:20:26 -0300

    feat: Adição classe de VeiculoCliente

---
+++ src/main/java/br/com/mcgauto/domain/entities/VeiculoCliente.java
@@ -0,0 +1,84 @@
+package br.com.mcgauto.domain.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "veiculo_cliente")
+public class VeiculoCliente {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long idVeiculo;
+    private Usuario dono;
+    private String marca;
+    private String nome;
+    private String placa;
+    private String modelo;
+
+    public VeiculoCliente() {}
+
+    public VeiculoCliente(long idVeiculo, Usuario dono, String marca, String nome, String placa, String modelo) {
+        this.idVeiculo = idVeiculo;
+        this.dono = dono;
+        this.marca = marca;
+        this.nome = nome;
+        this.placa = placa;
+        this.modelo = modelo;
+    }
+
+    public long getIdVeiculo() {
+        return idVeiculo;
+    }
+
+    public Usuario getDono() {
+        return dono;
+    }
+
+    public void setDono(Usuario dono) {
+        this.dono = dono;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "VeiculoCliente{" +
+                "idVeiculo=" + idVeiculo +
+                ", dono=" + dono +
+                ", marca='" + marca + '\'' +
+                ", nome='" + nome + '\'' +
+                ", placa='" + placa + '\'' +
+                ", modelo='" + modelo + '\'' +
+                '}';
+    }
+}
