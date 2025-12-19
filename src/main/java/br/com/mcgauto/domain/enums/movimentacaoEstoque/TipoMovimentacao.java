94f5eb710cc219acd7fc07d600e675985e39b238
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2025-12-18 23:01:44 -0300

    feat: implementação do enum TipoMovimentação

---
+++ src/main/java/br/com/mcgauto/domain/enums/movimentacaoEstoque/TipoMovimentacao.java
@@ -0,0 +1,10 @@
+package br.com.mcgauto.domain.enums.movimentacaoEstoque;
+
+public enum TipoMovimentacao {
+    ENTRADA_COMPRA,
+    SAIDA_VENDA,
+    SAIDA_SERVICO,
+    AJUSTE_POSITIVO,
+    AJUSTE_NEGATIVO,
+    DEVOLUCAO
+}
