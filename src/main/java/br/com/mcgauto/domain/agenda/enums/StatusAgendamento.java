1d8d24a64991115ed9b37c8ca34fe5fb9f2583c4
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2025-12-20 16:57:17 -0300

    feat: implementaçao enum de Status de Agendamento

---
+++ src/main/java/br/com/mcgauto/domain/agenda/enums/StatusAgendamento.java
@@ -0,0 +1,10 @@
+package br.com.mcgauto.domain.agenda.enums;
+
+public enum StatusAgendamento {
+    SOLICITADO,
+    CONFIRMADO,
+    CANCELADO_CLIENTE,
+    CANCELADO_EMPRESA,
+    FINALIZADO,
+    NAO_COMPARECEU
+}
