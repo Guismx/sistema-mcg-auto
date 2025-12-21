47cc87db7aadc48a4a717956db74613a3e6b983f
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2025-12-20 22:55:27 -0300

    feat: implementa aenum de Status do veículo

---
+++ src/main/java/br/com/mcgauto/domain/veiculo/enums/StatusVeiculo.java
@@ -0,0 +1,11 @@
+package br.com.mcgauto.domain.veiculo.enums;
+
+public enum StatusVeiculo {
+    DISPONIVEL,
+    RESERVADO_COMPRA,
+    RESERVADO_ALUGUEL,
+    VENDIDO,
+    ALUGADO,
+    MANUTENCAO,
+    ARQUIVADO
+}
