98555f883b151576159dbab41b187853f5438bfd
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2025-12-20 22:58:38 -0300

    feat: implementa enum StatusVenda

---
+++ src/main/java/br/com/mcgauto/domain/venda/enums/StatusVenda.java
@@ -0,0 +1,14 @@
+package br.com.mcgauto.domain.venda.enums;
+
+public enum StatusVenda {
+    PENDENTE_PAGAMENTO,
+    AGUARDANDO_APROVACAO,
+    PAGAMENTO_APROVADO,
+    APROVADA,
+    REGISTRADA,
+    FATURADA,
+    ENVIADA,
+    ENTREGUE,
+    CONCLUIDA,
+    CANCELADA
+}
