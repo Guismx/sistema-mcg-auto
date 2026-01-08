f40cc7f1b09c0057f39c05fd50e6176de63a48c9
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2026-01-08 00:45:57 -0300

    refactor: adiciona validação e ajusta construtor e toString

--- src/main/java/br/com/mcgauto/domain/veiculo/Veiculo.java
+++ src/main/java/br/com/mcgauto/domain/veiculo/Veiculo.java
@@ -5,6 +5,7 @@ import br.com.mcgauto.domain.veiculo.enums.StatusVeiculo;
 import br.com.mcgauto.domain.veiculo.enums.TipoCombustivel;
 import br.com.mcgauto.domain.veiculo.enums.TipoPropriedade;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -16,30 +17,74 @@ public class Veiculo {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private long id;
-    private int modeloId;
+
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn (name = "modelo_id", nullable = false)
+    private ModeloVeiculo modeloId;
+
+    @NotNull (message = "O ano do veículo deve ser informado")
+    @Positive
     private int anoModelo;
+
+    @NotBlank (message = "A cor deve ser informada")
+    @Size (min = 3, max = 20)
     private String cor;
+
+    @Min(7)
+    @NotBlank (message = "O número da placa deve ser informado")
+    @Column (length = 7, nullable = false, unique = true)
     private String placa;
+
+    @NotBlank (message = "O chassi deve ser informado")
     private String chassi;
     private String tipoModelo;
+
+    @NotNull (message = "A quilometragem deve ser informada")
+    @PositiveOrZero
     private int quilometragem;
+
+    @NotNull (message = "O tipo de combustivel deve ser informado")
+    @Enumerated (EnumType.STRING)
     private TipoCombustivel tipoCombustivel;
+
+    @NotNull
     private BigDecimal precoCusto;
+
+    @NotNull
     private BigDecimal precoVenda;
+
+    @NotNull (message = "O estado do veículo deve ser informado")
+    @Enumerated (EnumType.STRING)
     private EstadoVeiculo estadoVeiculo;
+
+    @NotNull (message = "O status do veículo deve ser informado")
+    @Enumerated (EnumType.STRING)
     private StatusVeiculo statusVeiculo;
+
+    @NotNull (message = "O tipo de propriedade deve ser informado")
+    @Enumerated (EnumType.STRING)
     private TipoPropriedade tipoPropriedade;
     private String descricaoDetalhada;
+
+    @NotNull
     private LocalDateTime dataEntradaEstoque;
+
+    @NotNull
     private LocalDateTime dataBaixaEstoque;
+
+    @NotNull
     private LocalDateTime criadoEm;
+
+    @NotNull
     private LocalDateTime atualizadoEm;
 
+    public Veiculo() {}
 
-    public Veiculo() {
-    }
-
-    public Veiculo(long id, int modeloId, int anoModelo, String cor, String placa, String chassi, String tipoModelo, int quilometragem, BigDecimal precoCusto, BigDecimal precoVenda, String descricaoDetalhada, LocalDateTime dataEntradaEstoque, LocalDateTime dataBaixaEstoque, LocalDateTime criadoEm, LocalDateTime atualizadoEm) {
+    public Veiculo(long id, ModeloVeiculo modeloId, int anoModelo, String cor, String placa, String chassi, String tipoModelo,
+                   int quilometragem, TipoCombustivel tipoCombustivel, BigDecimal precoCusto, BigDecimal precoVenda,
+                   EstadoVeiculo estadoVeiculo, StatusVeiculo statusVeiculo, TipoPropriedade tipoPropriedade,
+                   String descricaoDetalhada, LocalDateTime dataEntradaEstoque, LocalDateTime dataBaixaEstoque,
+                   LocalDateTime criadoEm, LocalDateTime atualizadoEm) {
         this.id = id;
         this.modeloId = modeloId;
         this.anoModelo = anoModelo;
@@ -48,8 +93,12 @@ public class Veiculo {
         this.chassi = chassi;
         this.tipoModelo = tipoModelo;
         this.quilometragem = quilometragem;
+        this.tipoCombustivel = tipoCombustivel;
         this.precoCusto = precoCusto;
         this.precoVenda = precoVenda;
+        this.estadoVeiculo = estadoVeiculo;
+        this.statusVeiculo = statusVeiculo;
+        this.tipoPropriedade = tipoPropriedade;
         this.descricaoDetalhada = descricaoDetalhada;
         this.dataEntradaEstoque = dataEntradaEstoque;
         this.dataBaixaEstoque = dataBaixaEstoque;
@@ -61,11 +110,11 @@ public class Veiculo {
         return id;
     }
 
-    public int getModeloId() {
+    public ModeloVeiculo getModeloId() {
         return modeloId;
     }
 
-    public void setModeloId(int modeloId) {
+    public void setModeloId(ModeloVeiculo modeloId) {
         this.modeloId = modeloId;
     }
 
@@ -117,6 +166,14 @@ public class Veiculo {
         this.quilometragem = quilometragem;
     }
 
+    public TipoCombustivel getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public void setTipoCombustivel(TipoCombustivel tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
     public BigDecimal getPrecoCusto() {
         return precoCusto;
     }
@@ -133,6 +190,30 @@ public class Veiculo {
         this.precoVenda = precoVenda;
     }
 
+    public EstadoVeiculo getEstadoVeiculo() {
+        return estadoVeiculo;
+    }
+
+    public void setEstadoVeiculo(EstadoVeiculo estadoVeiculo) {
+        this.estadoVeiculo = estadoVeiculo;
+    }
+
+    public StatusVeiculo getStatusVeiculo() {
+        return statusVeiculo;
+    }
+
+    public void setStatusVeiculo(StatusVeiculo statusVeiculo) {
+        this.statusVeiculo = statusVeiculo;
+    }
+
+    public TipoPropriedade getTipoPropriedade() {
+        return tipoPropriedade;
+    }
+
+    public void setTipoPropriedade(TipoPropriedade tipoPropriedade) {
+        this.tipoPropriedade = tipoPropriedade;
+    }
+
     public String getDescricaoDetalhada() {
         return descricaoDetalhada;
     }
@@ -145,8 +226,16 @@ public class Veiculo {
         return dataEntradaEstoque;
     }
 
+    public void setDataEntradaEstoque(LocalDateTime dataEntradaEstoque) {
+        this.dataEntradaEstoque = dataEntradaEstoque;
+    }
+
     public LocalDateTime getDataBaixaEstoque() {
         return dataBaixaEstoque;
+    }
+
+    public void setDataBaixaEstoque(LocalDateTime dataBaixaEstoque) {
+        this.dataBaixaEstoque = dataBaixaEstoque;
     }
 
     public LocalDateTime getCriadoEm() {
@@ -157,24 +246,31 @@ public class Veiculo {
         return atualizadoEm;
     }
 
+    public void setAtualizadoEm(LocalDateTime atualizadoEm) {
+        this.atualizadoEm = atualizadoEm;
+    }
+
     @Override
     public String toString() {
         return "Veiculo{" +
                 "id=" + id +
-                ", modeloId=" + modeloId +
                 ", anoModelo=" + anoModelo +
                 ", cor='" + cor + '\'' +
                 ", placa='" + placa + '\'' +
                 ", chassi='" + chassi + '\'' +
                 ", tipoModelo='" + tipoModelo + '\'' +
                 ", quilometragem=" + quilometragem +
+                ", tipoCombustivel=" + tipoCombustivel +
                 ", precoCusto=" + precoCusto +
                 ", precoVenda=" + precoVenda +
-                ", descricaoDetalhada='" + descricaoDetalhada + '\'' +
+                ", estadoVeiculo=" + estadoVeiculo +
+                ", statusVeiculo=" + statusVeiculo +
+                ", tipoPropriedade=" + tipoPropriedade +
                 ", dataEntradaEstoque=" + dataEntradaEstoque +
                 ", dataBaixaEstoque=" + dataBaixaEstoque +
                 ", criadoEm=" + criadoEm +
                 ", atualizadoEm=" + atualizadoEm +
                 '}';
     }
+
 }
