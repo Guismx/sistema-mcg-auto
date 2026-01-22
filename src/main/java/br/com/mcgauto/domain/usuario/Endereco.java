58a8b00eccafeca3baf8fdeef19d7594acf61424
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2026-01-22 01:05:28 -0300

    fix(usuario): corrigir modelagem de Endereco e permitir multiplos enderecos por usuario

--- src/main/java/br/com/mcgauto/domain/usuario/Endereco.java
+++ src/main/java/br/com/mcgauto/domain/usuario/Endereco.java
@@ -3,9 +3,10 @@ package br.com.mcgauto.domain.usuario;
 
 import br.com.mcgauto.domain.usuario.enums.TipoEndereco;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "enderecos")
@@ -14,35 +15,59 @@ public class Endereco {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private long id;
-    @OneToOne (fetch = FetchType.LAZY)
+
+    @ManyToOne (fetch = FetchType.LAZY)
     @JoinColumn (name = "usuario_id", nullable = false)
     private Usuario proprietario;
+
     @Enumerated (EnumType.STRING)
     @Column (name = "tipo_endereco", nullable = false)
     private TipoEndereco tipoEndereco;
-    private String logradouro;
-    private int numero;
-    private String complemento;
-    private String bairro;
-    private String cidade;
+
+    @Column (nullable = false, length = 9)
     private String cep;
+
+    @Column(nullable = false)
+    private String logradouro;
+
+    @Column(nullable = false, length = 10)
+    private String numero;
+
+    private String complemento;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false, length = 2)
+    private String uf;
+
+    @CreationTimestamp
+    @Column(name = "criado_em", updatable = false)
+    private LocalDateTime criadoEm;
+
+    @UpdateTimestamp
+    @Column(name = "atualizado_em")
+    private LocalDateTime atualizadoEm;
+
     private boolean ativo;
-    //TODO: Implementar UF
 
-    public Endereco(){}
+    public Endereco(){
+    }
 
-    public Endereco(long id, Usuario proprietario, TipoEndereco tipoEndereco, String logradouro, int numero,
-                    String complemento, String bairro, String cidade, String cep, boolean ativo) {
-        this.id = id;
+    public Endereco(Usuario proprietario, TipoEndereco tipoEndereco, String cep, String logradouro,
+                    String numero, String bairro, String cidade, String uf) {
         this.proprietario = proprietario;
         this.tipoEndereco = tipoEndereco;
+        this.cep = cep;
         this.logradouro = logradouro;
         this.numero = numero;
-        this.complemento = complemento;
         this.bairro = bairro;
         this.cidade = cidade;
-        this.cep = cep;
-        this.ativo = ativo;
+        this.uf = uf;
+        this.ativo = true;
     }
 
     public long getId() {
@@ -53,38 +78,37 @@ public class Endereco {
         return proprietario;
     }
 
-    public void setProprietario(Usuario proprietario) {
-        this.proprietario = proprietario;
-    }
-
     public TipoEndereco getTipoEndereco() {
         return tipoEndereco;
     }
-
     public void setTipoEndereco(TipoEndereco tipoEndereco) {
         this.tipoEndereco = tipoEndereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
     }
 
     public String getLogradouro() {
         return logradouro;
     }
-
     public void setLogradouro(String logradouro) {
         this.logradouro = logradouro;
     }
 
-    public int getNumero() {
+    public String getNumero() {
         return numero;
     }
-
-    public void setNumero(int numero) {
+    public void setNumero(String numero) {
         this.numero = numero;
     }
 
     public String getComplemento() {
         return complemento;
     }
-
     public void setComplemento(String complemento) {
         this.complemento = complemento;
     }
@@ -92,7 +116,6 @@ public class Endereco {
     public String getBairro() {
         return bairro;
     }
-
     public void setBairro(String bairro) {
         this.bairro = bairro;
     }
@@ -100,40 +123,35 @@ public class Endereco {
     public String getCidade() {
         return cidade;
     }
-
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
 
-    public String getCep() {
-        return cep;
+    public String getUf() {
+        return uf;
     }
-
-    public void setCep(String cep) {
-        this.cep = cep;
+    public void setUf(String uf) {
+        this.uf = uf;
     }
 
     public boolean isAtivo() {
         return ativo;
     }
-
     public void setAtivo(boolean ativo) {
         this.ativo = ativo;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
     }
 
     @Override
     public String toString() {
         return "Endereco{" +
                 "id=" + id +
-                ", proprietario=" + proprietario +
-                ", tipoEndereco=" + tipoEndereco +
-                ", logradouro='" + logradouro + '\'' +
-                ", numero=" + numero +
-                ", complemento='" + complemento + '\'' +
-                ", bairro='" + bairro + '\'' +
-                ", cidade='" + cidade + '\'' +
+                ", tipo=" + tipoEndereco +
                 ", cep='" + cep + '\'' +
-                ", ativo=" + ativo +
+                ", cidade='" + cidade + '/' + uf + '\'' +
                 '}';
     }
 }
