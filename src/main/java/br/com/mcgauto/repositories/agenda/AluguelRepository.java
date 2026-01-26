4e940cf170a855a9c6b0132bf33ee2019756b7c8
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2026-01-25 23:20:32 -0300

    feat(agenda): implementa filtros de busca

--- src/main/java/br/com/mcgauto/repositories/agenda/AluguelRepository.java
+++ src/main/java/br/com/mcgauto/repositories/agenda/AluguelRepository.java
@@ -1,9 +1,20 @@
 package br.com.mcgauto.repositories.agenda;
 
 import br.com.mcgauto.domain.agenda.Aluguel;
+import br.com.mcgauto.domain.veiculo.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface AluguelRepository extends JpaRepository <Aluguel, Long> {
+    List<Aluguel> findByClienteId(Long clienteId);
+    List<Veiculo> findByVeiculoId(Long veiculoId);
+
+    //Carros que saem hoje (Retirada)
+    List<Aluguel> findByDataRetiradaPrevista(LocalDate hoje);
+    //Carros que voltam hoje (Devolução)
+    List<Aluguel> findByDataDevolucaoPrevista(LocalDate hoje);
 }
