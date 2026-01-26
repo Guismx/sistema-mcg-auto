6509a81600278ce5acd235fe3b819f83b06f5c17
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2026-01-26 00:22:03 -0300

    fix(repository): corrige filtros de busca

--- src/main/java/br/com/mcgauto/repositories/estoque/MovimentacaoEstoqueRepository.java
+++ src/main/java/br/com/mcgauto/repositories/estoque/MovimentacaoEstoqueRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface MovimentacaoEstoqueRepository extends JpaRepository<MovimentacaoEstoque, Long> {
-    List<MovimentacaoEstoque> findByResponsavel (Long id);
-    List<MovimentacaoEstoque> findByTipoMovimentacaoEquals (TipoMovimentacao tipo);
-    List<MovimentacaoEstoque> findByDataHoraMovimentacaoEquals (LocalDate data);
+    List<MovimentacaoEstoque> findByResponsavelId (Long responsavelId);
+    List<MovimentacaoEstoque> findByTipoMovimentacao (TipoMovimentacao tipo);
+    List<MovimentacaoEstoque> findByDataHoraMovimentacaoBetween (LocalDateTime data);
 
 }
