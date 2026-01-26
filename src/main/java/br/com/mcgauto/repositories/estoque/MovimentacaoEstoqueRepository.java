ae222f4f1f2a5e079819d6982cd573d100080e4a
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2026-01-25 23:53:46 -0300

    feat: adiciona filtros por responsável, tipo e data no MovimentacaoEstoqueRepositor

--- src/main/java/br/com/mcgauto/repositories/estoque/MovimentacaoEstoqueRepository.java
+++ src/main/java/br/com/mcgauto/repositories/estoque/MovimentacaoEstoqueRepository.java
@@ -1,9 +1,17 @@
 package br.com.mcgauto.repositories.estoque;
 
 import br.com.mcgauto.domain.estoque.MovimentacaoEstoque;
+import br.com.mcgauto.domain.estoque.enums.TipoMovimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface MovimentacaoEstoqueRepository extends JpaRepository<MovimentacaoEstoque, Long> {
+    List<MovimentacaoEstoque> findByResponsavel (Long id);
+    List<MovimentacaoEstoque> findByTipoMovimentacaoEquals (TipoMovimentacao tipo);
+    List<MovimentacaoEstoque> findByDataHoraMovimentacaoEquals (LocalDate data);
+
 }
