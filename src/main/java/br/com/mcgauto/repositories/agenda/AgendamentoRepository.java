dec16c90371908892e984f3fd269ad32f996b4b4
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2026-01-25 23:38:02 -0300

    feat: adiciona filtros por cliente, serviço e status no AgendamentoRepository

--- src/main/java/br/com/mcgauto/repositories/agenda/AgendamentoRepository.java
+++ src/main/java/br/com/mcgauto/repositories/agenda/AgendamentoRepository.java
@@ -1,9 +1,16 @@
 package br.com.mcgauto.repositories.agenda;
 
 import br.com.mcgauto.domain.agenda.Agendamento;
+import br.com.mcgauto.domain.agenda.enums.StatusAgendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AgendamentoRepository extends JpaRepository <Agendamento, Long> {
+
+    List<Agendamento> findByClienteId (Long clienteId);
+    List<Agendamento> findByServico(Long servicoId);
+    List<Agendamento> findByStatusAgendamentoEquals(StatusAgendamento status);
 }
