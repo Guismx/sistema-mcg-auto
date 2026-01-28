704229cd20d19f9eb240db67fe6f42c79b5a5d5d
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2026-01-27 23:44:16 -0300

    feat(usuario): implementa filtros no UsuarioRepository

--- src/main/java/br/com/mcgauto/repositories/usuario/CnhRepository.java
+++ src/main/java/br/com/mcgauto/repositories/usuario/CnhRepository.java
@@ -4,6 +4,15 @@ import br.com.mcgauto.domain.usuario.Cnh;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CnhRepository extends JpaRepository<Cnh, Long> {
+
+    //Buscar pelo número da CNH
+    Optional<Cnh> findByNumeroCnh(String numeroCnh);
+
+    //Filtrar pela categoria
+    List<Cnh> findByCategoria(String categoria);
 }
