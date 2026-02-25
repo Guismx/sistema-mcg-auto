4d86041dc9103ad25a7119c6b600ce6ea6df4438
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2026-02-24 23:51:14 -0300

    feat(financeiro): implementa DTO's do pacote financeiro

---
+++ src/main/java/br/com/mcgauto/dto/financeiro/PagamentoCreateRequestDTO.java
@@ -0,0 +1,33 @@
+package br.com.mcgauto.dto.financeiro;
+
+import br.com.mcgauto.domain.financeiro.enums.StatusPagamento;
+import br.com.mcgauto.domain.financeiro.enums.TipoOrigem;
+import br.com.mcgauto.domain.financeiro.enums.TipoPagamento;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record PagamentoCreateRequestDTO(
+        Long vendaId,
+        Long aluguelId,
+
+        @NotNull (message = "Necessário a origem do pagamento")
+        TipoOrigem tipoOrigem,
+
+        @NotNull (message = "Data de pagamento obrigatório")
+        LocalDateTime dataPagamento,
+
+        @NotNull (message = "Obrigado informar o valor pago")
+        @Positive
+        BigDecimal valorPago,
+
+        @NotNull (message = "Obrigado informar a forma de pagamento")
+        TipoPagamento formaPagamento,
+
+        StatusPagamento statusPagamento,
+        String transacaoGateway,
+        int parcelas
+) {
+}
---
+++ src/main/java/br/com/mcgauto/dto/financeiro/PagamentoResponseDTO.java
@@ -0,0 +1,24 @@
+package br.com.mcgauto.dto.financeiro;
+
+import br.com.mcgauto.domain.financeiro.enums.StatusPagamento;
+import br.com.mcgauto.domain.financeiro.enums.TipoOrigem;
+import br.com.mcgauto.domain.financeiro.enums.TipoPagamento;
+import br.com.mcgauto.dto.venda.VendaResponseDTO;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record PagamentoResponseDTO(
+        Long id,
+        VendaResponseDTO venda,
+        AluguelResponseDTO aluguel,
+        OrdemServicoResponseDTO ordemServico,
+        TipoOrigem tipoOrigem,
+        LocalDateTime dataPagamento,
+        BigDecimal valorPago,
+        TipoPagamento tipoPagamento,
+        StatusPagamento statusPagamento,
+        String transacaoGateway,
+        int parcelas
+) {
+}
