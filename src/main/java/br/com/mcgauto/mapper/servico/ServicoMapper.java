e699298ccaf927b4f44b5aaf3642ae3bfd095a15
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2026-03-05 23:54:18 -0300

    feat: implementa mappers de list e atualizÃação da entidade servico

--- src/main/java/br/com/mcgauto/mapper/servico/ServicoMapper.java
+++ src/main/java/br/com/mcgauto/mapper/servico/ServicoMapper.java
@@ -4,10 +4,19 @@ import br.com.mcgauto.domain.servico.Servico;
 import br.com.mcgauto.dto.servico.ServicoRequestDTO;
 import br.com.mcgauto.dto.servico.ServicoResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper (componentModel = "spring")
+import java.util.List;
+
+@Mapper (componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ServicoMapper {
 
     Servico toEntity(ServicoRequestDTO dto);
+
     ServicoResponseDTO toResponseDTO(Servico servico);
+
+    List<ServicoResponseDTO> toResponseDTOList(List<Servico> servicos);
+
+    void updateEntityFromDTO(ServicoRequestDTO dto, @MappingTarget Servico servico);
 }
